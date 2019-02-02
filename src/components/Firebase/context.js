3f5b5021d020039187222f223bec96dbbd6b0bdf
SiennaS <ssienna48@gmail.com>
2019-02-02 13:59:03 -0500

    beginnings of firebase

---
+++ src/components/Firebase/context.js
@@ -0,0 +1,12 @@
+// is used to provide a Firebase instance once at the top-level
+// of your React component tree
+import FirebaseContext from './context';
+
+// retrieve the Firebase instance if it is needed in the React component
+import Firebase from './firebase';
+
+
+
+export default Firebase;
+
+export { FirebaseContext };
